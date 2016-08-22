ffbc849ec53c3d70e9de941f551bb40f1fcd58ab
Olaniyan Elias <elias.akin@gmail.com>
2016-08-22 16:50:59 +0100

    new Update
    This update lets you fetch search results from wikipedia using WIKI_api. 
    You need to install "Request module" before this works, this module was not added to reduce its size.
    run : node input 
    to use the application

--- noteapp/input.js
+++ noteapp/input.js
@@ -6,7 +6,7 @@ var int = r1.createInterface({
 	output: process.stdout
 })
 
-var options = { 
+var getOptions = { 
   method: 'GET',
   url: 'https://en.wikipedia.org/w/api.php',
   qs: 
@@ -15,21 +15,62 @@ var options = {
      prop: 'extracts',
      exintro: '',
      explaintext: '',
-     titles: 'alex ferguson' },
+     titles: '' },
   headers: 
    { 'postman-token': '3ba40789-049e-ff52-5948-b8bfe4e20e9f',
      'cache-control': 'no-cache' } 
  };
 
+var postOptions = { method: 'POST',
+  url: 'https://en.wikipedia.org/w/api.php',
+  qs: 
+   { format: 'json',
+     action: 'query',
+     prop: 'extracts',
+     exintro: '',
+     explaintext: '',
+     titles: '' },
+  headers: 
+   { 'postman-token': 'b4b2cac9-e257-b660-3d8d-005273a7ee5d',
+     'cache-control': 'no-cache' } };
+function parseJson(json){
+	extract = '';
+	for ( key in json.query.pages ){
+	 extract = json.query.pages[key].extract;
+	  break;
+	  }
+	  return extract;
+}
 int.question('Ok, welcome, what would you like to search in wikipedia? ', function(ans){
 	if(ans.length > 1){
-		options.qs.titles = encodeURIComponent(ans);
-		request(options, function (error, response, body) {
-	  		if (error) throw new Error(error);
-	  	console.log(body);
-		});
+		int.question('Do you want a secured search Y/N? ', function (reply){
+			if(reply == "Y"){
+				postOptions.qs.titles = encodeURI(ans);
+				request(postOptions, function (error, response, body) {
+	  				if (error) throw new Error(error);
+	  				body = JSON.parse(body);
+	  				console.log(`
+	  					----------------------#
+	  					Page Title: About ${ans}
+
+	  					${parseJson(body)}`);
+			});
+			}
+			else{
+				getOptions.qs.titles = encodeURI(ans);
+				request(getOptions, function (error, response, body) {
+	  				if (error) throw new Error(error);
+	  				body = JSON.parse(body);
+	  				console.log(`
+	  					----------------------#
+	  					Page Title: About ${ans}
+
+	  					${parseJson(body)}`);
+			});
+			}
+			});
 	}
 	else{
-		
+		console.log("Input is invalid");	
 	}
 });
